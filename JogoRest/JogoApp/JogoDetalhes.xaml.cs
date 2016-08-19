9b169cdf0e833f9b84ad5296af02c137dd7f033a
Helena <helenanc@uol.com.br>
2016-08-19 19:20:25 -0300

    JogoDetalhes = Design + Código

--- JogoRest/JogoApp/JogoDetalhes.xaml.cs
+++ JogoRest/JogoApp/JogoDetalhes.xaml.cs
@@ -22,11 +22,15 @@ namespace JogoApp
     public partial class JogoDetalhes : Window
     {
         private static Models.Jogo jogo { get; set; }
+        private static Models.MeuJogo meu { get; set; }
+
+        private int media;
 
         public JogoDetalhes(Models.Jogo j)
         {
             InitializeComponent();
             jogo = new Models.Jogo();
+            meu = new Models.MeuJogo();
             jogo = j;
             PopularPag();
             SetGenero(j.IdGenero);
@@ -74,6 +78,45 @@ namespace JogoApp
                     lbPlataforma.Items.Add(p.Descricao);
                 }
             }
+        }
+            
+        private async void SetMeuJogo(int idJogo, string status)
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(ip);
+            var response = await httpClient.GetAsync("/api/Usuario/");
+            var str = response.Content.ReadAsStringAsync().Result;
+            List<Models.Usuario> obj = JsonConvert.DeserializeObject<List<Models.Usuario>>(str);
+            Models.Usuario usr = obj.Find(x => x.EstaAutenticado == true);
+
+            meu.IdUsuario = usr.Id;
+            meu.IdJogo = idJogo;
+            meu.Status = status;
+            meu.Classificacao = media.ToString();
+        }
+
+        private void btnQueroJogar_Click(object sender, RoutedEventArgs e)
+        {
+            SetMeuJogo(jogo.Id, "Quero Jogar");
+        }
+
+        private void btnJogando_Click(object sender, RoutedEventArgs e)
+        {
+            SetMeuJogo(jogo.Id, "Jogando");
+        }
+
+        private void btnJaJoguei_Click(object sender, RoutedEventArgs e)
+        {
+            SetMeuJogo(jogo.Id, "Já Joguei");
+        }
+
+        private void btnAvaliar_Click(object sender, RoutedEventArgs e)
+        {
+            if (radioButton1.IsChecked == true) media = 1;
+            if (radioButton2.IsChecked == true) media = 2;
+            if (radioButton3.IsChecked == true) media = 3;
+            if (radioButton4.IsChecked == true) media = 4;
+            if (radioButton5.IsChecked == true) media = 5;
         }
     }
 }
